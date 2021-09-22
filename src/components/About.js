b6eb84c80bcb0c3fb75f35bac2b4681f8d11337f
troymeeker <troymeeker1@gmail.com>
2021-09-22 10:52:08 -0700

    complete

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>A late learner of coding, I have completely shifted careers in order to create a better future for myself and my future family. And loving every day of it!</p>
+    <img src={image} alt="I made this"></img>
+  </div>;
 }
 
 export default About;
